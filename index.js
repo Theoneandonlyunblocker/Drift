580cd2453be3f95ab150e4906af474ebae9fd485
Jimmy Neutron <83769295+SmartCoder3000@users.noreply.github.com>
2022-03-14 19:09:14 +0000

    More Games And WIP Javascript Rewriter

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const request = require('request')
 var fs = require('fs')
 var express = require('express')
 var app = express()
+var device = require('express-device');
 var hbs = require('hbs')
 const rewriter = require('./lib/rewriter.js')
 var config = require('./app.json')
 
+app.use(device.capture());
 app.set('view engine', 'hbs');
 
 const blockedSites = config.blacklist
@@ -14,194 +16,202 @@ var proxyPath = config.prefix
 const port = process.env.PORT || config.port
 
 function getcookie(req) {
-  const parseCookie = str =>
-    str
-    .split(';')
-    .map(v => v.split('='))
-    .reduce((acc, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+	const parseCookie = str =>
+		str
+			.split(';')
+			.map(v => v.split('='))
+			.reduce((acc, v) => {
+				acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
 
-      return acc;
-    }, {});
+				return acc;
+			}, {});
 
-  var cookie = req.headers.cookie;
-  cookie.toString()
+	var cookie = req.headers.cookie;
+	cookie.toString()
 
-  cookie = parseCookie(cookie)
+	cookie = parseCookie(cookie)
 
-  return cookie['proxyUrl']
+	return cookie['proxyUrl']
 }
 
 function isPreset(url) {
-  var xCurl = url.replace('https://', '')
+	var xCurl = url.replace('https://', '')
 
 }
 
-app.get(`${proxyPath}/*`, function (req, res) {
-  //res.cookie('proxypath', proxyPath).send('cookie set');
-  try {
+app.get(`${proxyPath}/*`, function(req, res) {
+	//res.cookie('proxypath', proxyPath).send('cookie set');
+	try {
 
-    var proxyUrl = req.originalUrl.substr(proxyPath.length + 1, req.originalUrl.length)
+		var proxyUrl = req.originalUrl.substr(proxyPath.length + 1, req.originalUrl.length)
 
-    proxyUrl = proxyUrl.replace("https:/", "")
-    proxyUrl = proxyUrl.replace("http:/", "")
+		proxyUrl = proxyUrl.replace("https:/", "")
+		proxyUrl = proxyUrl.replace("http:/", "")
 
-    proxyUrl = `https://${proxyUrl}`
+		proxyUrl = `https://${proxyUrl}`
 
-    if (!(blockedSites.includes(proxyUrl))) {
+		if (!(blockedSites.includes(proxyUrl))) {
+			const options = {
+				url: proxyUrl,
+				headers: 'user-agent: Mozilla Firefox Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0',
+				method: 'GET'
+			};
+			request(options, function(error, response, body) {
+				if (error) {
+					res.send(error.toString())
+				} else {
 
-      request(proxyUrl, function (error, response, body) {
-        if (error) {
-          res.send(error.toString())
-        } else {
-
-          if (!(typeof response.caseless.get('Content-Type') === "undefined")) {
-            var contentType = response.caseless.get('Content-Type')
-          } else {
-            var contentType = "text/css"
-          }
+					if (!(typeof response.caseless.get('Content-Type') === "undefined")) {
+						var contentType = response.caseless.get('Content-Type')
+					} else {
+						var contentType = "text/css"
+					}
 
 
-          if (!(typeof contentType === "undefined")) {
-            res.set({
-              'Content-Type': response.caseless.get('Content-Type')
-            })
-          } else {
-            res.set({
-              "Content-Type": 'text/css'
-            })
-          }
+					if (!(typeof contentType === "undefined")) {
+						res.set({
+							'Content-Type': response.caseless.get('Content-Type')
+						})
+					} else {
+						res.set({
+							"Content-Type": 'text/css'
+						})
+					}
 
-          if (contentType.includes("image")) {
-            request({
-                url: url,
-                encoding: null
-              },
-              (err, respond, buffer) => {
-                res.setHeader('Content-Type',contentType)
-                res.end()
-              });
-          } else {
-            if (contentType.includes("text/html")) {
-              res.send(rewriter.write(body, proxyUrl, response.caseless.get('Content-Type')));
-            } else {
-              res.send(body)
-            }
-          }
-        }
-      });
-    } else {
-      res.render('blocked', {
-        site: proxyUrl
-      })
-    }
-  } catch (e) {
-    console.log(e)
-    res.sendFile('500.html', {
-      "root": __dirname + "/views"
-    })
-  }
+					if (contentType.includes("image")) {
+						request({
+							url: url,
+							encoding: null
+						},
+							(err, respond, buffer) => {
+								res.setHeader('Content-Type', contentType)
+								res.send(Buffer.from(response.body))
+							});
+					} else {
+						res.send(rewriter.write(body, proxyUrl, response.caseless.get('Content-Type')));
+					}
+				}
+			});
+		} else {
+			res.render('blocked', {
+				site: proxyUrl
+			})
+		}
+	} catch (e) {
+		console.log(e)
+		res.render('500',{err:e})
+	}
 });
 
-app.post('/', function (req, res) {
-  var proxyUrl = getcookie(req).toString()
+app.post('/', function(req, res) {
+	var proxyUrl = getcookie(req).toString()
 
-  proxyUrl = proxyUrl.replace("https:/", "")
-  proxyUrl = proxyUrl.replace("http:/", "")
+	proxyUrl = proxyUrl.replace("https:/", "")
+	proxyUrl = proxyUrl.replace("http:/", "")
 
-  proxyUrl = `https://${proxyUrl}`
+	proxyUrl = `https://${proxyUrl}`
+	try {
+	if (!(blockedSites.includes(proxyUrl))) {
+		request(proxyUrl, function(error, response, body) {
 
+			if (error) {
+				res.send(error)
+			} else {
 
-  request(proxyUrl, function (error, response, body) {
+				if (!(typeof response.caseless.get('Content-Type') === "undefined")) {
+					var contentType = response.caseless.get('Content-Type')
+				} else {
+					var contentType = "text/css"
+				}
 
-    if (error) {
-      res.send(error)
-    } else {
+				if (!(typeof contentType === "undefined")) {
+					res.set({
+						'Content-Type': response.caseless.get('Content-Type')
+					})
+				} else {
+					res.set({
+						"Content-Type": 'text/css'
+					})
+				}
 
-      if (!(typeof response.caseless.get('Content-Type') === "undefined")) {
-        var contentType = response.caseless.get('Content-Type')
-      } else {
-        var contentType = "text/css"
-      }
+				if (contentType.includes("image")) {
+					request({
+						url: url,
+						encoding: null
+					},
+						(err, resp, buffer) => {
+							res.set({
+								"Content-Type": contentType
+							});
 
-      if (!(typeof contentType === "undefined")) {
-        res.set({
-          'Content-Type': response.caseless.get('Content-Type')
-        })
-      } else {
-        res.set({
-          "Content-Type": 'text/css'
-        })
-      }
+							res.send(Buffer.from(response.body));
 
-      if (contentType.includes("image")) {
-        request({
-            url: url,
-            encoding: null
-          },
-          (err, resp, buffer) => {
-            res.set({
-              "Content-Type": contentType
-            });
-
-            res.send(Buffer.from(response.body));
-
-          });
-      } else {
-        if (contentType.includes("text/html")) {
-          res.send(rewriter.write(body, proxyUrl, response.caseless.get('Content-Type')));
-        } else {
-          res.send(body)
-        }
-      }
-    }
-  });
+						});
+				} else {
+					res.send(rewriter.write(body, proxyUrl, response.caseless.get('Content-Type')));
+				}
+			}
+		})
+	} else {
+		res.render('blocked', {
+			site: proxyUrl
+		})
+	}} catch (e){
+		res.render('500',{err:e})
+	}
 });
 
-app.get('/', function (req, res) {
-  res.sendFile("index.html", {
-    "root": __dirname + "/views"
-  })
+app.get('/', function(req, res) {
+	var devType = req.device.type
+	if (devType === "desktop") {
+		res.sendFile("index.html", {
+			"root": __dirname + "/views"
+		})
+	} else {
+		res.sendFile("mobile.html", {
+			"root": __dirname + "/views"
+		})
+	}
 })
 
-app.get('/content/*', function (req, res) {
-  let url = req.originalUrl.substr(9, req.originalUrl.length)
-  res.sendFile('siteLib/' + url, {
-    "root": __dirname
-  })
+app.get('/content/*', function(req, res) {
+	let url = req.originalUrl.substr(9, req.originalUrl.length)
+	res.sendFile('siteLib/' + url, {
+		"root": __dirname
+	})
 })
 
-app.get('/games', function (req, res) {
-  res.sendFile('games.html', {
-    "root": __dirname + "/views"
-  })
+app.get('/games', function(req, res) {
+	res.sendFile('games.html', {
+		"root": __dirname + "/views"
+	})
 })
 
-app.get('/game/:game', function (req, res) {
-  var game = req.params.game
-  res.sendFile(`./siteLib/games/${game.toLowerCase()}.html`, {
-    "root": __dirname
-  })
+app.get('/game/:game', function(req, res) {
+	var game = req.params.game
+	res.sendFile(`./siteLib/games/${game.toLowerCase()}.html`, {
+		"root": __dirname
+	})
 })
 
-app.get('/bookmarks', function (req, res) {
-  res.sendFile('bookmarklets.html', {
-    "root": __dirname + "/views"
-  })
+app.get('/bookmarks', function(req, res) {
+	res.sendFile('bookmarklets.html', {
+		"root": __dirname + "/views"
+	})
 })
 
-app.get('/cloak',function(req,res){
-  res.sendFile('tabCloak.html', {
-    "root": __dirname + "/views"
-  })
+app.get('/cloak', function(req, res) {
+	res.sendFile('tabCloak.html', {
+		"root": __dirname + "/views"
+	})
 })
 
-app.get('/*', function (req, res) {
-  res.sendFile("404.html", {
-    "root": __dirname + "/views"
-  })
+app.get('/*', function(req, res) {
+	res.sendFile("404.html", {
+		"root": __dirname + "/views"
+	})
 })
 
-app.listen(port, '0.0.0.0', function () {
-  console.log(`Drift is running on 0.0.0.0:${port}`)
+app.listen(port, '0.0.0.0', function() {
+	console.log(`Drift is running on 0.0.0.0:${port}`)
 })
--- siteLib/js/onpage.js
+++ siteLib/js/onpage.js
@@ -1,103 +1,118 @@
 var updatedCheck = false
 
 function validUrl(trUrl) {
-  var regexpression = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+	var regexpression = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
-  if (regexpression.test(trUrl)) {
-    return true
-  } else {
-    return false
-  }
+	if (regexpression.test(trUrl)) {
+		return true
+	} else {
+		return false
+	}
 }
 
 function tabCloak() {
-  var favicon = getCookie('faviconCloak')
-  var titleCloal = getCookie('titleCloak')
-  
-  console.log(favicon)
+	var favicon = getCookie('faviconCloak')
+	var titleCloal = getCookie('titleCloak')
 
-  if (!(typeof favicon === "null")) {
-    favChange(favicon)
-  }
+	console.log(favicon)
 
-  if (!(typeof titleCloal === "null")) {
-    document.title = titleCloal
-  }
+	if (!(typeof favicon === "null")) {
+		favChange(favicon)
+	}
+
+	if (!(typeof titleCloal === "null")) {
+		document.title = titleCloal
+	}
 }
 
 function favChange(link) {
-  let $favicon = document.querySelector('link[rel="icon"]')
+	let $favicon = document.querySelector('link[rel="icon"]')
 
-  if ($favicon !== null) {
-    $favicon.href = link
+	if ($favicon !== null) {
+		$favicon.href = link
 
-  } else {
-    $favicon = document.createElement("link")
-    $favicon.rel = "icon"
-    $favicon.href = link
-    document.head.appendChild($favicon)
-  }
+	} else {
+		$favicon = document.createElement("link")
+		$favicon.rel = "icon"
+		$favicon.href = link
+		document.head.appendChild($favicon)
+	}
 }
 
 function getCookie(cName) {
-  const name = cName + "=";
-  const cDecoded = decodeURIComponent(document.cookie); //to be careful
-  const cArr = cDecoded.split('; ');
-  let res;
-  cArr.forEach(val => {
-    if (val.indexOf(name) === 0) res = val.substring(name.length);
-  })
-  return res
+	const name = cName + "=";
+	const cDecoded = decodeURIComponent(document.cookie); //to be careful
+	const cArr = cDecoded.split('; ');
+	let res;
+	cArr.forEach(val => {
+		if (val.indexOf(name) === 0) res = val.substring(name.length);
+	})
+	return res
 }
 
-var observeDOM = (function () {
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observeDOM = (function() {
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-  return function (obj, callback) {
-    if (!obj || obj.nodeType !== 1) return;
+	return function(obj, callback) {
+		if (!obj || obj.nodeType !== 1) return;
 
-    if (MutationObserver) {
+		if (MutationObserver) {
 
-      var mutationObserver = new MutationObserver(callback)
+			var mutationObserver = new MutationObserver(callback)
 
-      mutationObserver.observe(obj, {
-        childList: true,
-        subtree: true
-      })
-      return mutationObserver
-    } else if (window.addEventListener) {
-      obj.addEventListener('DOMNodeInserted', callback, false)
-      obj.addEventListener('DOMNodeRemoved', callback, false)
-    }
-  }
+			mutationObserver.observe(obj, {
+				childList: true,
+				subtree: true
+			})
+			return mutationObserver
+		} else if (window.addEventListener) {
+			obj.addEventListener('DOMNodeInserted', callback, false)
+			obj.addEventListener('DOMNodeRemoved', callback, false)
+		}
+	}
 })()
 
 async function update() {
-  updatedCheck = true
+	updatedCheck = true
 
-  var DRIFTelements = document.querySelectorAll("*")
+	var DRIFTelements = document.querySelectorAll("*")
 
-  for (let i = 0; i < DRIFTelements.length; i++) {
-    var DRIFTelm = DRIFTelements[i]
-    var DriftHref = DRIFTelm.getAttribute('drift-href')
-    var DriftSrc = DRIFTelm.getAttribute('drift-src')
-    var DriftAction = DRIFTelm.getAttribute('drift-action')
+	for (let i = 0; i < DRIFTelements.length; i++) {
+		var DRIFTelm = DRIFTelements[i]
+		var DriftHref = DRIFTelm.getAttribute('drift-href')
+		var DriftSrc = DRIFTelm.getAttribute('drift-src')
+		var DriftAction = DRIFTelm.getAttribute('drift-action')
+		var href = DRIFTelm.getAttribute('href')
+		var src = DRIFTelm.getAttribute('src')
+		var action = DRIFTelm.getAttribute('action')
 
-    //DRIFTelm.removeAttribute('integrity')
-
-    if (DriftHref) {
-      DRIFTelm.href = DriftHref
-    }
-    if (DriftSrc) {
-      DRIFTelm.src = DriftSrc
-    }
-    if (DriftAction) {
-      DRIFTelm.action = DriftAction
-    }
-  }
-  tabCloak()
+		if (!(DriftHref === null)) {
+			DRIFTelm.href = DriftHref
+		} else {
+			if ((!(href === null)) && DriftHref === null) {
+				DRIFTelm.setAttribute('drift-href', href)
+			}
+		}
+		if (!(DriftSrc === null)) {
+			DRIFTelm.src = DriftSrc
+		} else {
+			if ((!(src === null)) && DriftSrc === null) {
+				DRIFTelm.setAttribute('drift-src', src)
+			}
+		}
+		if (!(DriftAction === null)) {
+			DRIFTelm.action = DriftAction
+		} else {
+			if ((!(action === null)) && DriftAction === null) {
+				DRIFTelm.setAttribute('drift-action', action)
+			}
+		}
+	}
+	tabCloak()
 }
 
-observeDOM(document, function () {
-  update()
-})
+update()
+
+setInterval(function() {
+	update()
+}, 3000)
